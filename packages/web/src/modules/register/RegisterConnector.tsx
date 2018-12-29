fa92874c97e5de42f1bc9dfd0c7fbb35f4f0c0dc
AlexanderLapygin <AlexanderLapygin@gmail.com>
2018-12-30 00:53:05 +0300

    feat: add formik and corresponding app code

--- packages/web/src/modules/register/RegisterConnector.tsx
+++ packages/web/src/modules/register/RegisterConnector.tsx
@@ -1,4 +1,14 @@
 import * as React from "react";
-import RegisterView from "./ui/RegisterView";
+import {RegisterView} from "./ui/RegisterView";
 
-export const RegisterConnector = () => <RegisterView />;
+export class RegisterConnector extends React.PureComponent {
+
+  dummySubmit = async (values: any) => {
+    console.log(values);
+    return null;
+  }
+
+  render() {
+    return <RegisterView submit={this.dummySubmit} />;
+  }
+}

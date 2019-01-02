3dc1b1335817d67e43e1a3b169464bec1ad95a56
AlexanderLapygin <AlexanderLapygin@gmail.com>
2019-01-02 11:46:17 +0300

    feat: add controller package (common for web - React and mobile - React Native)

---
+++ packages/controller/src/modules/RegisterController/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export class RegisterController extends React.PureComponent {
+  render() {
+    return (
+      <div>hello from controllers</div>
+    )
+  }
+}
--- packages/web/src/modules/register/RegisterConnector.tsx
+++ packages/web/src/modules/register/RegisterConnector.tsx
@@ -1,14 +1,20 @@
 import * as React from "react";
-import {RegisterView} from "./ui/RegisterView";
+import { RegisterController } from "@cap-cross/controller";
+
+import { RegisterView } from "./ui/RegisterView";
 
 export class RegisterConnector extends React.PureComponent {
-
   dummySubmit = async (values: any) => {
     console.log(values);
     return null;
-  }
+  };
 
   render() {
-    return <RegisterView submit={this.dummySubmit} />;
+    return (
+      <React.Fragment>
+        <RegisterController />
+        <RegisterView submit={this.dummySubmit} />;
+      </React.Fragment>
+    );
   }
 }
